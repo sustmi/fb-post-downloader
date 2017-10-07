18952097903936725ea0aea3ae1f082d1fd48ee5
Miroslav Sustek <sustmidown@centrum.cz>
2017-10-07 21:20:07 +0200

    Implement FacebookRemotePostRepository using FacebookSDK

--- src/Model/Post/FacebookRemotePostRepository.php
+++ src/Model/Post/FacebookRemotePostRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Post;
 
 use DateTime;
+use Facebook\Facebook;
 
 class FacebookRemotePostRepository implements RemotePostRepository
 {
@@ -13,22 +14,76 @@ class FacebookRemotePostRepository implements RemotePostRepository
     private $pageId;
 
     /**
-     * @param string $pageId
+     * @var Facebook
      */
-    public function __construct(string $pageId)
-    {
+    private $facebook;
+
+    public function __construct(
+        string $pageId,
+        Facebook $facebook
+    ) {
         $this->pageId = $pageId;
+        $this->facebook = $facebook;
     }
 
     public function getPostsCreatedFrom(DateTime $createdFrom): array
     {
-        // TODO: implement
-        return [];
+        $endpoint = sprintf(
+            '/%s/posts?limit=100&since=%d&until=%d',
+            $this->pageId,
+            $createdFrom->getTimestamp() - 1,
+            (new DateTime())->getTimestamp()
+        );
+
+        $response = $this->facebook->get($endpoint);
+        $edge = $response->getGraphEdge();
+
+        $posts = [];
+        do {
+            foreach ($edge->asArray() as $item) {
+                $posts[] = $this->createPostFromItemData($item);
+            }
+
+            $edge = $this->facebook->next($edge);
+        } while ($edge !== null);
+
+        return $posts;
     }
 
     public function getLastPosts(int $count): array
     {
-        // TODO: implement
-        return [];
+        $endpoint = sprintf('/%s/posts?limit=100', $this->pageId);
+
+        $response = $this->facebook->get($endpoint);
+        $edge = $response->getGraphEdge();
+
+        $posts = [];
+        do {
+            foreach ($edge->asArray() as $item) {
+                $posts[] = $this->createPostFromItemData($item);
+
+                if (count($posts) >= $count) {
+                    return $posts;
+                }
+            }
+
+            $edge = $this->facebook->next($edge);
+        } while ($edge !== null);
+
+        return $posts;
+    }
+
+    /**
+     * @param array $item
+     * @return Post
+     */
+    private function createPostFromItemData(array $item): Post
+    {
+        return new Post(
+            $item['id'],
+            $item['created_time'],
+            $item['message'] ?? null,
+            $item['story'] ?? null
+        );
     }
 }
--- src/Model/Post/FacebookRemotePostRepositoryFactory.php
+++ src/Model/Post/FacebookRemotePostRepositoryFactory.php
@@ -4,11 +4,38 @@ declare(strict_types=1);
 namespace App\Model\Post;
 
 use App\Model\Page\Page;
+use Facebook\Facebook;
 
 class FacebookRemotePostRepositoryFactory
 {
+    /**
+     * @var string
+     */
+    private $appId;
+
+    /**
+     * @var string
+     */
+    private $appSecret;
+
+    public function __construct(string $appId, string $appSecret)
+    {
+        $this->appId = $appId;
+        $this->appSecret = $appSecret;
+    }
+
     public function createForPage(Page $page): FacebookRemotePostRepository
     {
-        return new FacebookRemotePostRepository($page->getId());
+        $facebook = new Facebook([
+            'app_id' => $this->appId,
+            'app_secret' => $this->appSecret,
+            'default_graph_version' => 'v2.10',
+            'default_access_token' => $this->appId . '|' . $this->appSecret,
+        ]);
+
+        return new FacebookRemotePostRepository(
+            $page->getId(),
+            $facebook
+        );
     }
 }
