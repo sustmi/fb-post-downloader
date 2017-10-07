c3c3ec9d22ed7dc5b21613a6ab020185f1bcbfbd
Miroslav Sustek <sustmidown@centrum.cz>
2017-10-07 21:19:49 +0200

    Post: make $message optional and add $story attribute

--- src/Model/Post/Post.php
+++ src/Model/Post/Post.php
@@ -18,13 +18,25 @@ class Post
     private $createdAt;
 
     /**
-     * @param string $id
-     * @param DateTime $createdAt
+     * @var string|null
      */
-    public function __construct(string $id, DateTime $createdAt)
-    {
+    private $message;
+
+    /**
+     * @var string|null
+     */
+    private $story;
+
+    public function __construct(
+        string $id,
+        DateTime $createdAt,
+        string $message = null,
+        string $story = null
+    ) {
         $this->id = $id;
         $this->createdAt = $createdAt;
+        $this->message = $message;
+        $this->story = $story;
     }
 
     public function getId(): string
@@ -35,5 +47,15 @@ class Post
     public function getCreatedAt(): DateTime
     {
         return $this->createdAt;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function getStory(): ?string
+    {
+        return $this->story;
     }
 }
