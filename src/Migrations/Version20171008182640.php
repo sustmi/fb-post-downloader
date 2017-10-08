58322ecd6e58304a73bf2b2c84dd94f396d8e10c
Miroslav Sustek <sustmidown@centrum.cz>
2017-10-08 18:57:55 +0200

    DB: add index to posts.created_at for faster retrieval of the last posts

---
+++ src/Migrations/Version20171008182640.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171008182640 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $posts = $schema->getTable('posts');
+        $posts->addIndex(['created_at']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
